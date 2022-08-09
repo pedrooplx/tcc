12d8a1ce742f20e4fa225a692aaacbf19304a06e
Pedro Henrique <pedrooplx@gmail.com>
2022-08-09 13:49:56 -0300

    Initial

---
+++ TCC.Infra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCC.Infra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
